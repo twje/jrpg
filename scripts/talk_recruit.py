dc910e52755b80d1494f51ad13791fedf094a171
Thomas Edwards <thomas.w.j.edwards@gmail.com>
2021-06-13 10:09:33 +1000

    remove trigger on recruit

--- scripts/talk_recruit.py
+++ scripts/talk_recruit.py
@@ -13,11 +13,12 @@ def talk_recruit(map, trigger, entity, layer, x, y):
     stack = context.data["stack"]
     world = context.data["world"]
     npc = map.get_npc(x, y, layer)
-    actor_def = PartyModel()[npc.actor_id]
+    actor_def = PartyModel()[npc.actor_id]    
 
     def on_recruit(index, item):
         world.party.add(Actor(actor_def))
         map.remove_npc(x, y, layer)
+        map.remove_trigger(x, y, layer)
 
     stack.push_fitted(
         info.screen_width/2,
